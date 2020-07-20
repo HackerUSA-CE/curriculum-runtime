f523800949190fb7d2353ea31f90db9a8d59df4f
Joshua Miles <joshua.m@hackerusa.com>
2020-07-20 10:39:33 -0500

    -

--- index.js
+++ index.js
@@ -3,8 +3,22 @@ const components = {
     'multiple-choice': (question) => {
         let { questionId } = question.dataset
         let answerButtons = question.querySelectorAll(`[data-answer-for="${questionId}"]`)
-        console.log(answerButtons)
+        let feedbackDiv = question.querySelector(`[data-feedback-for="${questionId}"]`)
+        for(let answerButton of answerButtons){
+            let { correct, feedback } = getMetadata(answerButton)
+            answerButton.addEventListener('click', () => {
+                for(let answerButton of answerButtons) answerButton.disabled = true
+                feedbackDiv.innerText = `${correct ? "Correct!" : "Incorrect"}
+                ${feedback}`
+            })
+        }
     }
+}
+
+const getMetadata = (element) => {
+    const { metadataIndex } = element.dataset
+    if(metadataIndex === undefined) return {}
+    else return window.metadata[metadataIndex]
 }
 
 const register = () => {
