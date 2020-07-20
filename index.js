a1c8ad43fed048b69a36cfa6bf152171fbb0c7e5
Joshua Miles <joshua.m@hackerusa.com>
2020-07-20 10:33:55 -0500

    -

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('Hello world')
+
+const components = {
+    'multiple-choice': (question) => {
+        let { questionId } = question.dataset
+        let answerButtons = question.querySelectorAll(`[data-answer-for="${questionId}"]`)
+        console.log(answerButtons)
+    }
+}
+
+const register = () => {
+    for(let componentName in components){
+        let component = components[componentName]
+        let elements = document.querySelectorAll(`[data-component="${componentName}"]`)
+        for(let element of elements){
+            component(element)
+        }
+    }
+}
+register()
